a9dc2b91047c3f012417e41af71b75b48994ef29
Oscar Marin <oscar@marin.cr>
2023-11-30 14:55:37 -0600

    improvements

---
+++ code/Luval.Framework.Core/Configuration/ConfigManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.Framework.Core.Configuration
+{
+    public static class ConfigManager
+    {
+        private static IConfigurationProvider? _config;
+
+        public static void Init(IConfigurationProvider configurationProvider)
+        {
+
+            _config = configurationProvider ?? throw new ArgumentNullException(nameof(configurationProvider));
+        }
+
+        /// <summary>
+        /// Gets the setting
+        /// </summary>
+        /// <param name="name">The name for the setting to retrieve</param>
+        /// <returns>The value for the setting</returns>
+        public static string Get(string name)
+        {
+            return GetOrDefault(name, null);
+        }
+
+
+        /// <summary>
+        /// Gets the setting, if not found returns a default value
+        /// </summary>
+        /// <param name="name">The name for the setting to retrieve</param>
+        /// <param name="defaultValue">A default value in case the setting is not available</param>
+        /// <returns>The value for the setting</returns>
+        public static string GetOrDefault(string name, string defaultValue)
+        {
+            if (_config == null) throw new Exception("Instance not initialized, invoke the Init method");
+            return _config.GetOrDefault(name, defaultValue);
+        }
+
+
+    }
+}
